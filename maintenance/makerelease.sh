5f5df5ba1c44d9f2ab85b8d1dbbd8b656e0502a2
Michael Buesch <m@bues.ch>
2025-04-12 16:40:36 +0200

    Convert Readme to MD

--- maintenance/makerelease.sh
+++ maintenance/makerelease.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
+# -*- coding: utf-8 -*-
 
-srcdir="$(dirname "$0")"
-[ "$(echo "$srcdir" | cut -c1)" = '/' ] || srcdir="$PWD/$srcdir"
-
+srcdir="$(realpath "$0" | xargs dirname)"
 srcdir="$srcdir/.."
 
 # Import the makerelease.lib
 # https://bues.ch/cgit/misc.git/tree/makerelease.lib
 die() { echo "$*"; exit 1; }
 for path in $(echo "$PATH" | tr ':' ' '); do
-	[ -f "$MAKERELEASE_LIB" ] && break
-	MAKERELEASE_LIB="$path/makerelease.lib"
+    [ -f "$MAKERELEASE_LIB" ] && break
+    MAKERELEASE_LIB="$path/makerelease.lib"
 done
 [ -f "$MAKERELEASE_LIB" ] && . "$MAKERELEASE_LIB" || die "makerelease.lib not found."
 
+hook_get_version()
+{
+    version="$(cargo_local_pkg_version rangelockrs)"
+}
+
 project=rangelockrs
 makerelease "$@"
+
+# vim: ts=4 sw=4 expandtab
